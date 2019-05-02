a5b75375bd8bb9e106ce6b3def51f71c90ce47a3
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-02 13:48:19 +0200

    Update 06_bismark_genome_preparation.sh

--- 00_scripts/06_bismark_genome_preparation.sh
+++ 00_scripts/06_bismark_genome_preparation.sh
@@ -5,10 +5,12 @@
 
 
 DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
-GENOME=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
+#GENOME=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/sspace.final.scaffolds.fasta
+GENOME=/home1/datawork/plstenge/Pearl_Oyster_Colour_Population_Genomics/01_data/
 BISMARK_ENV=". /appli/bioinfo/bismark/0.19/env.sh" #version 0.19
 
 $BISMARK_ENV
-cd $DATADIRECTORY
+#cd $DATADIRECTORY
+cd $GENOME
 
 bismark_genome_preparation $GENOME
