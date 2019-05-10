6a8384d88cc858ac49c9b7c9ec829b7874849de3
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-10 13:18:14 +0200

    Update 07_bismark_alignment.sh

--- 00_scripts/07_bismark_alignment.sh
+++ 00_scripts/07_bismark_alignment.sh
@@ -4,11 +4,10 @@
 #PBS -l select=1:ncpus=28:mem=115g
 
 
-DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/99_09_test_bismark
+DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
 READS=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/05_concatenated
 GENOME=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/01_genome
-#OUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
-OUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/99_09_test_bismark
+OUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
 
 BISMARK_ENV=". /appli/bioinfo/bismark/0.19/env.sh" #version 0.19
 
@@ -25,6 +24,23 @@ BISMARK_ENV=". /appli/bioinfo/bismark/0.19/env.sh" #version 0.19
 
 $BISMARK_ENV
 cd $DATADIRECTORY
-#cd $GENOME
 
-bismark -l 32 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_10.2-V-620_R1_paired.fastq.gz -2 $READS/Index_10.2-V-620_R2_paired.fastq.gz > $OUTPUT/Index_10.2-V-620.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_1.3-R-180_R1_paired.fastq.gz -2 $READS/Index_1.3-R-180_R2_paired.fastq.gz   > $OUTPUT/Index_1.3-R-180.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_10.2-V-620_R1_paired.fastq.gz -2 $READS/Index_10.2-V-620_R2_paired.fastq.gz > $OUTPUT/Index_10.2-V-620.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_11.3-V-620_R1_paired.fastq.gz -2 $READS/Index_11.3-V-620_R2_paired.fastq.gz > $OUTPUT/Index_11.3-V-620.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_18.1-J-2_R1_paired.fastq.gz -2 $READS/Index_18.1-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_18.1-J-2.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_19.2-J-2_R1_paired.fastq.gz -2 $READS/Index_19.2-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_19.2-J-2.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_2.1-R-183_R1_paired.fastq.gz -2 $READS/Index_2.1-R-183_R2_paired.fastq.gz   > $OUTPUT/Index_2.1-R-183.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_20.3-J-2_R1_paired.fastq.gz -2 $READS/Index_20.3-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_20.3-J-2.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_21.1-J-7_R1_paired.fastq.gz -2 $READS/Index_21.1-J-7_R2_paired.fastq.gz     > $OUTPUT/Index_21.1-J-7.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_22.2-J-7_R1_paired.fastq.gz -2 $READS/Index_22.2-J-7_R2_paired.fastq.gz     > $OUTPUT/Index_22.2-J-7.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_23.3-J-7_R1_paired.fastq.gz -2 $READS/Index_23.3-J-7_R2_paired.fastq.gz     > $OUTPUT/Index_23.3-J-7.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_25.1-R-180_R1_paired.fastq.gz -2 $READS/Index_25.1-R-180_R2_paired.fastq.gz > $OUTPUT/Index_25.1-R-180.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_27.2-R-180_R1_paired.fastq.gz -2 $READS/Index_27.2-R-180_R2_paired.fastq.gz > $OUTPUT/Index_27.2-R-180.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_3.2-R-183_R1_paired.fastq.gz -2 $READS/Index_3.2-R-183_R2_paired.fastq.gz   > $OUTPUT/Index_3.2-R-183.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_5.3-R-183_R1_paired.fastq.gz -2 $READS/Index_5.3-R-183_R2_paired.fastq.gz   > $OUTPUT/Index_5.3-R-183.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_6.1-V-613_R1_paired.fastq.gz -2 $READS/Index_6.1-V-613_R2_paired.fastq.gz   > $OUTPUT/Index_6.1-V-613.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_7.2-V-613_R1_paired.fastq.gz -2 $READS/Index_7.2-V-613_R2_paired.fastq.gz   > $OUTPUT/Index_7.2-V-613.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_8.3-V-613_R1_paired.fastq.gz -2 $READS/Index_8.3-V-613_R2_paired.fastq.gz   > $OUTPUT/Index_8.3-V-613.sam
+bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_9.1-V-620_R1_paired.fastq.gz -2 $READS/Index_9.1-V-620_R2_paired.fastq.gz   > $OUTPUT/Index_9.1-V-620.sam 
+
