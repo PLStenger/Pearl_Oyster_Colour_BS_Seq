d23f034dd3579cfd43ba59ecca2a9daca4803133
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-13 09:40:18 +0200

    Update 07_bismark_alignment.sh

--- 00_scripts/07_bismark_alignment.sh
+++ 00_scripts/07_bismark_alignment.sh
@@ -25,14 +25,14 @@ BISMARK_ENV=". /appli/bioinfo/bismark/0.19/env.sh" #version 0.19
 $BISMARK_ENV
 cd $DATADIRECTORY
 
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_1.3-R-180_R1_paired.fastq.gz -2 $READS/Index_1.3-R-180_R2_paired.fastq.gz   > $OUTPUT/Index_1.3-R-180.sam
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_10.2-V-620_R1_paired.fastq.gz -2 $READS/Index_10.2-V-620_R2_paired.fastq.gz > $OUTPUT/Index_10.2-V-620.sam
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_11.3-V-620_R1_paired.fastq.gz -2 $READS/Index_11.3-V-620_R2_paired.fastq.gz > $OUTPUT/Index_11.3-V-620.sam
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_18.1-J-2_R1_paired.fastq.gz -2 $READS/Index_18.1-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_18.1-J-2.sam
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_19.2-J-2_R1_paired.fastq.gz -2 $READS/Index_19.2-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_19.2-J-2.sam
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_2.1-R-183_R1_paired.fastq.gz -2 $READS/Index_2.1-R-183_R2_paired.fastq.gz   > $OUTPUT/Index_2.1-R-183.sam
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_20.3-J-2_R1_paired.fastq.gz -2 $READS/Index_20.3-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_20.3-J-2.sam
-bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_21.1-J-7_R1_paired.fastq.gz -2 $READS/Index_21.1-J-7_R2_paired.fastq.gz     > $OUTPUT/Index_21.1-J-7.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_1.3-R-180_R1_paired.fastq.gz -2 $READS/Index_1.3-R-180_R2_paired.fastq.gz   > $OUTPUT/Index_1.3-R-180.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_10.2-V-620_R1_paired.fastq.gz -2 $READS/Index_10.2-V-620_R2_paired.fastq.gz > $OUTPUT/Index_10.2-V-620.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_11.3-V-620_R1_paired.fastq.gz -2 $READS/Index_11.3-V-620_R2_paired.fastq.gz > $OUTPUT/Index_11.3-V-620.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_18.1-J-2_R1_paired.fastq.gz -2 $READS/Index_18.1-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_18.1-J-2.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_19.2-J-2_R1_paired.fastq.gz -2 $READS/Index_19.2-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_19.2-J-2.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_2.1-R-183_R1_paired.fastq.gz -2 $READS/Index_2.1-R-183_R2_paired.fastq.gz   > $OUTPUT/Index_2.1-R-183.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_20.3-J-2_R1_paired.fastq.gz -2 $READS/Index_20.3-J-2_R2_paired.fastq.gz     > $OUTPUT/Index_20.3-J-2.sam
+#bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_21.1-J-7_R1_paired.fastq.gz -2 $READS/Index_21.1-J-7_R2_paired.fastq.gz     > $OUTPUT/Index_21.1-J-7.sam
 bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_22.2-J-7_R1_paired.fastq.gz -2 $READS/Index_22.2-J-7_R2_paired.fastq.gz     > $OUTPUT/Index_22.2-J-7.sam
 bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_23.3-J-7_R1_paired.fastq.gz -2 $READS/Index_23.3-J-7_R2_paired.fastq.gz     > $OUTPUT/Index_23.3-J-7.sam
 bismark -l 30 -n 0 -p 4 -q --genome $GENOME -1 $READS/Index_25.1-R-180_R1_paired.fastq.gz -2 $READS/Index_25.1-R-180_R2_paired.fastq.gz > $OUTPUT/Index_25.1-R-180.sam
