581b5f75dc073cabc5637bb6e920b950952440dd
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-15 09:04:16 +0200

    Update 08_bismark_methylation_extractor.sh

--- 00_scripts/08_bismark_methylation_extractor.sh
+++ 00_scripts/08_bismark_methylation_extractor.sh
@@ -22,14 +22,33 @@ $BISMARK_ENV
 
 # bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_10.2-V-620_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
 
-bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_11.3-V-620_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+# bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_11.3-V-620_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
 
-bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_1.3-R-180_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+# bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_1.3-R-180_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
 
-bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_18.1-J-2_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+# bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_18.1-J-2_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
 
-bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_19.2-J-2_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+# bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_19.2-J-2_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
 
-bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_20.3-J-2_R1_paired_bismark_bt2_pe.bam-o $OUTPUT
+# bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_20.3-J-2_R1_paired_bismark_bt2_pe.bam-o $OUTPUT
 
-bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_21.1-J-7_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+# bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_21.1-J-7_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_22.2-J-7_R1_paired_bismark_bt2_pe.bam-o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_23.3-J-7_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_25.1-R-180_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_27.2-R-180_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_3.2-R-183_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_5.3-R-183_R1_paired_bismark_bt2_pe.bam-o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_6.1-V-613_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+
+bismark_methylation_extractor -p --no_overlap --bedGraph --scaffolds --cytosine_report --genome_folder $GENOME --multicore $NCPU $BAM/Index_7.2-V-613_R1_paired_bismark_bt2_pe.bam -o $OUTPUT
+
+
+
