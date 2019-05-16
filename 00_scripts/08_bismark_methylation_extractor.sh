16a9fb1964248e0c206a5fec344ebb0f9c2c7723
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-16 09:22:43 +0200

    Update 08_bismark_methylation_extractor.sh

--- 00_scripts/08_bismark_methylation_extractor.sh
+++ 00_scripts/08_bismark_methylation_extractor.sh
@@ -8,12 +8,14 @@ DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/07_bismark_methylat
 READS=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/05_concatenated
 GENOME=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/01_genome
 BAM=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
-OUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/07_bismark_methylation_extractor
+#OUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/07_bismark_methylation_extractor
+OUTPUT=/home1/scratch/plstenge/bismark_methylation_extractor
 #PATHTOFILE=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/07_bismark_methylation_extractor/output_extractor
 BISMARK_ENV=". /appli/bioinfo/bismark/0.19/env.sh" #version 0.19
 NCPU=4
 
-cd $DATADIRECTORY
+#cd $DATADIRECTORY
+cd $OUTPUT
 $BISMARK_ENV
 
 #bismark_methylation_extractor -s --bedGraph --counts --buffer_size 10G -- cytosine_report --genome_folder $GENOME Index_10.2-V-620_R1_paired_bismark_bt2_pe.bam
