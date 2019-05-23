cb7fb8fa882742e9c24de270d86361c01f667710
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-05-23 13:26:31 +0200

    Update 10_methylkit_CpG_calling.R

--- 00_scripts/10_methylkit_CpG_calling.R
+++ 00_scripts/10_methylkit_CpG_calling.R
@@ -10,20 +10,25 @@ setwd("/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark")
 
 # A set of BAM or methylation call files #
 
-list.bam=list("Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam",
-                "Index_11.3-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam",
-                "Index_6.1-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam",
-                "Index_7.2-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam",
-                "Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam",
-                "Index_9.1-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam")
+#list.bam=list("Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam",
+#                "Index_11.3-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam",
+#                "Index_6.1-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam",
+#                "Index_7.2-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam",
+#                "Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam",
+#                "Index_9.1-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam")
+
+list.bam=list("Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam",
+                "Index_11.3-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam")
 
 
-list.id=list("2-V-620",
-                "3-V-620",
-                "1-V-613",
-                "2-V-613",
-                "3-V-613",
-                "1-V-620")
+#list.id=list("2-V-620",
+#                "3-V-620",
+#                "1-V-613",
+#                "2-V-613",
+#                "3-V-613",
+#                "1-V-620")
+
+list.id=list("2-V-620", "3-V-620")
 
 myallmeth=processBismarkAln(location=list.bam,
                 sample.id=list.id,
@@ -32,7 +37,7 @@ myallmeth=processBismarkAln(location=list.bam,
                 save.context=c("CpG"),
                 read.context="CpG",
                 mincov=10,
-                treatment=c(0,0,1,1,1,0))
+                treatment=c(0,1))
 
 #myallmeth=methRead(list.file, sample.id = list.id, assembly = "trimmed_genome_v1", treatment = c(0,1,0,1,1,0,1,0), context = "CpG")
 
