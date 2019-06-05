1d84ad261a875aa61d1da1766664cc6cc4426826
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-06-05 19:17:58 +0200

    Create 10_02_clean_sam.sh

---
+++ 00_scripts/10_02_clean_sam.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+
+DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
+
+sed 's/|size/_/g' Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.sam > Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.sam
+sed 's/|size/_/g' Index_7.2-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.sam >  Index_7.2-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.sam
+sed 's/|size/_/g' Index_6.1-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted.bam.sam > Index_6.1-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.sam
+
