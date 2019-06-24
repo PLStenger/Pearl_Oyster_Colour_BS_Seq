b462a6574622a88b1508ea0a019068122174e9e2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-06-24 11:35:26 +0200

    Update 13_intersect_diff_methyl_gff.sh

--- 00_scripts/13_intersect_diff_methyl_gff.sh
+++ 00_scripts/13_intersect_diff_methyl_gff.sh
@@ -3,9 +3,9 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/09_annotation
+DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
+OUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/09_annotation
 GFF=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/09_annotation/sspace.final.scaffolds_no_pipe_size.gff3
-INPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/09_annotation/Inversed_3V620_3J2_Diffmeth.norm_all_control_color_against_all_treatment_color_time_1_diff25.q0.001.hyper_ok.txt
 BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.26.0/env.sh"
 
 # https://bedtools.readthedocs.io/en/latest/content/tools/intersect.html
@@ -14,5 +14,15 @@ cd $DATADIRECTORY
 
 $BEDTOOLS_ENV
 
-bedtools intersect -a $GFF -b $INPUT > output_test.txt
+for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05*.hyp*.txt)
 
+do
+
+tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
+awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+
+bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
+
+done;
+
+#rm *_no_first_line*
