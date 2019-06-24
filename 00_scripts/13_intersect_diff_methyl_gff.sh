14c72ec57ee826e1ac9d93f04677b1eafcaa48d8
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-06-24 11:44:47 +0200

    Update 13_intersect_diff_methyl_gff.sh

--- 00_scripts/13_intersect_diff_methyl_gff.sh
+++ 00_scripts/13_intersect_diff_methyl_gff.sh
@@ -19,7 +19,7 @@ for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05*.hyp*.tx
 do
 
 tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
-awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
 
 bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
 
