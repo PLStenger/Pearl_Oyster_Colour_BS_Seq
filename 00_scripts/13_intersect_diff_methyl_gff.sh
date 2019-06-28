57fc76d6e2194edc2df14e4466575ea5baf376e3
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-06-28 13:34:21 +0200

    Update 13_intersect_diff_methyl_gff.sh

--- 00_scripts/13_intersect_diff_methyl_gff.sh
+++ 00_scripts/13_intersect_diff_methyl_gff.sh
@@ -15,7 +15,19 @@ cd $DATADIRECTORY
 $BEDTOOLS_ENV
 
 #for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05*.interest*.txt)
-for FILE in $(ls $DATADIRECTORY/Diffmeth.norm_Inversed_3V620_3J2_color_battle_all*interest_diff25.q0.001.txt)
+#for FILE in $(ls $DATADIRECTORY/Diffmeth.norm_Inversed_3V620_3J2_color_battle_all*interest_diff25.q0.001.txt)
+for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_treatment_color_*time_3.txt)
+
+do
+
+tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
+awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+
+bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
+
+done;
+
+for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_treatment_color_*time_2.txt)
 
 do
 
