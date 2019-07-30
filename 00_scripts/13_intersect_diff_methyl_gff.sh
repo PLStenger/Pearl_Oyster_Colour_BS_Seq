02f11fcb811f99aed6bd015446c3d6668bfca92b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-07-30 15:25:02 +0200

    Update 13_intersect_diff_methyl_gff.sh

--- 00_scripts/13_intersect_diff_methyl_gff.sh
+++ 00_scripts/13_intersect_diff_methyl_gff.sh
@@ -3,7 +3,8 @@
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=28:mem=115g
 
-DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
+#DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
+DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/99_tests
 OUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/09_annotation
 GFF=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/09_annotation/sspace.final.scaffolds_no_pipe_size.gff3
 BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.26.0/env.sh"
@@ -18,28 +19,29 @@ $BEDTOOLS_ENV
 #for FILE in $(ls $DATADIRECTORY/Diffmeth.norm_Inversed_3V620_3J2_color_battle_all*interest_diff25.q0.001.txt)
 #for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_treatment_color_*time_3.txt)
 #for FILE in $(ls $DATADIRECTORY/Diffmeth.norm_Inversed_3V620_3J2_color_battle_all_*yellow.txt)
-for FILE in $(ls $DATADIRECTORY/*_cpg_count.txt)
-
-do
-
-tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
-sed 's/"//g' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
-awk '{print $2"\t"".""\t""transcrit""\t"$4"\t"$5}' ${FILE##*/}_no_first_line_rearranged.txt > ${FILE##*/}_no_first_line_rearranged2.txt
-
-mac2unix ${FILE##*/}_no_first_line_rearranged2.txt
-
-bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged2.txt > $OUTPUT/${FILE##*/}_annotated.txt
-
-done;
-
+for FILE in $(ls $DATADIRECTORY/*txt)
+#for FILE in $(ls $DATADIRECTORY/*_cpg_count.txt)
+#
 #do
 #
 #tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
-#awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+#sed 's/"//g' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+#awk '{print $2"\t"".""\t""transcrit""\t"$4"\t"$5}' ${FILE##*/}_no_first_line_rearranged.txt > ${FILE##*/}_no_first_line_rearranged2.txt
 #
-#bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
+#mac2unix ${FILE##*/}_no_first_line_rearranged2.txt
+#
+#bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged2.txt > $OUTPUT/${FILE##*/}_annotated.txt
 #
 #done;
+#
+do
+
+tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
+awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+
+bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
+
+done;
 #
 #for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_control_color_time_*2.txt)
 #
