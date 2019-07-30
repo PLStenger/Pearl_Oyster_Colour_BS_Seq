b7898ab7115984f18141959ed8b4939a028b96ef
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-07-30 13:19:09 +0200

    Update 13_intersect_diff_methyl_gff.sh

--- 00_scripts/13_intersect_diff_methyl_gff.sh
+++ 00_scripts/13_intersect_diff_methyl_gff.sh
@@ -18,26 +18,36 @@ $BEDTOOLS_ENV
 #for FILE in $(ls $DATADIRECTORY/Diffmeth.norm_Inversed_3V620_3J2_color_battle_all*interest_diff25.q0.001.txt)
 #for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_treatment_color_*time_3.txt)
 #for FILE in $(ls $DATADIRECTORY/Diffmeth.norm_Inversed_3V620_3J2_color_battle_all_*yellow.txt)
-for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_control_color_time_*3.txt)
+for FILE in $(ls $DATADIRECTORY/_cpg_count.txt)
 
 do
 
 tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
-awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+sed 's/""//g' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
 
 bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
 
 done;
 
-for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_control_color_time_*2.txt)
-
-do
-
-tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
-awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
-
-bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
-
-done;
-
+#do
+#
+#tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
+#awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+#
+#bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
+#
+#done;
+#
+#for FILE in $(ls $DATADIRECTORY/Inversed_3V620_3J2_Diffmeth.norm_Q_0_05_all_control_color_time_*2.txt)
+#
+#do
+#
+#tail -n +2 ${FILE##*/} > ${FILE##*/}_no_first_line.txt
+#awk '{print $2"\t"".""\t""transcrit""\t"$3"\t"$4"\t"$6"\t"$7"\t"$8}' ${FILE##*/}_no_first_line.txt > ${FILE##*/}_no_first_line_rearranged.txt
+#
+#bedtools intersect -a $GFF -b $DATADIRECTORY/${FILE##*/}_no_first_line_rearranged.txt > $OUTPUT/${FILE##*/}_annotated.txt
+#
+#done;
+#
 #rm *_no_first_line*
+
