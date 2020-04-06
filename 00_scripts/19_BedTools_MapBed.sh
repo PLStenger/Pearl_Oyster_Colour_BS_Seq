fa2d88a4d05198c1796bb316af813e292a4029de
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-06 11:46:57 +1100

    Update 19_BedTools_MapBed.sh

--- 00_scripts/19_BedTools_MapBed.sh
+++ 00_scripts/19_BedTools_MapBed.sh
@@ -5,7 +5,7 @@
 
 DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
 DATAOUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
-CORRESPONDANCE=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/01_genome/Correspondance_scaff_07.txt
+CORRESPONDANCE=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/01_genome/Correspondance_scaff_07.gff
 BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.26.0/env.sh" # https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
 
 cd $DATADIRECTORY
