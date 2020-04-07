d7cc85dc07d17c43266568880890d08855b90cde
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-07 11:15:11 +1100

    Add files via upload

---
+++ 00_scripts/20_GBM/GBM_14.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+setwd("/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark")
+
+dat <- read.table("Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.bed_correspondance.bed_base.txt")
+
+for(i in 1:length(unique(dat$V1))){
+  sink("Index_8.3-V-613_GBM.txt", append=TRUE)
+  pos_name <- as.character(unique(dat$V1)[i])
+  newdata <- dat[ which(dat$V1==pos_name), ]
+  print(c(pos_name, mean(newdata$V2)))
+  sink()
+}
