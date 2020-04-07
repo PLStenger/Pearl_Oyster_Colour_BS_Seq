66e1ff3523e58b73babb05e68ea6e791580e8580
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-07 12:13:32 +1100

    Create 21_crack_the_genecode.sh

---
+++ 00_scripts/21_crack_the_genecode.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
+DATAOUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
+BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.26.0/env.sh" # https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
+
+cd $DATADIRECTORY
+
+$BEDTOOLS_ENV
+
+bedtools intersect -a 3-V-620_cpg_count_info_02.txt -b gencode_02.txt -wa -wb > 3-V-620_cpg_count_gencode.txt
