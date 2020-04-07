30f064497c8bca5665b2f84b664ceedd561a1b5f
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-08 09:56:54 +1100

    Update 19_02_BedTools_MapBed.sh

--- 00_scripts/19_02_BedTools_MapBed.sh
+++ 00_scripts/19_02_BedTools_MapBed.sh
@@ -6,7 +6,7 @@
 DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
 DATAOUTPUT=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
 CORRESPONDANCE=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/01_genome/Correspondance_scaff_07.gff
-BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.26.0/env.sh" # https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
+BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.27.1/env.sh" # https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
 
 cd $DATADIRECTORY
 
@@ -20,16 +20,19 @@ $BEDTOOLS_ENV
 # OK
 #bedtools intersect -b Index_9.1-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bed -a Correspondance_scaff_07.gff -wa -wb > Index_9.1-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam_correspondance.bed
 
-for FILE in $(ls $DATADIRECTORY/*clean.bam.bed)
-do
-        bedtools intersect -b ${FILE##*/} -a Correspondance_scaff_07.gff -wa -wb > ${FILE##*/}_correspondance.bed
-done ;
-
-for FILE in $(ls $DATADIRECTORY/*_correspondance.bed)
-do
-       awk '{print $4" "$10}' ${FILE##*/}  > ${FILE##*/}_correspondance_02.bed
-       sed 's/ /\t/g' ${FILE##*/}_correspondance_02.bed > ${FILE##*/}_base.txt
-done ;
+#for FILE in $(ls $DATADIRECTORY/*clean.bam.bed)
+#do
+#        bedtools intersect -b ${FILE##*/} -a Correspondance_scaff_07.gff -wa -wb > ${FILE##*/}_correspondance.bed
+#done ;
+#
+#for FILE in $(ls $DATADIRECTORY/*_correspondance.bed)
+#do
+#       awk '{print $4" "$10}' ${FILE##*/}  > ${FILE##*/}_correspondance_02.bed
+#       sed 's/ /\t/g' ${FILE##*/}_correspondance_02.bed > ${FILE##*/}_base.txt
+#done ;
 
 #rm *_correspondance*
+
+bedtools map -a 1-J-2_cpg_count_gencode_02.bed -b Correspondance_scaff_07.gff > 1-J-2_cpg_count_gencode_02_correspondance.bed
+
 
