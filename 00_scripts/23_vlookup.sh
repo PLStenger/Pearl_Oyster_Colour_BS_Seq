653df78ffb3ac0fac65055353e7490dc76c510df
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-08 12:59:41 +1100

    Update 23_vlookup.sh

--- 00_scripts/23_vlookup.sh
+++ 00_scripts/23_vlookup.sh
@@ -13,3 +13,156 @@ awk '
 ($1 in a) {                # lookup from transcriptome
     print
 }' Correspondance_scaff_08.txt 1-J-2_correspondance_02.bed_GBM.txt > 1-J-2_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 1-V-620_correspondance_02.bed_GBM.txt > 1-V-620_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 2-V-613_correspondance_02.bed_GBM.txt > 2-V-613_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 3-R-183_correspondance_02.bed_GBM.txt > 3-R-183_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 1-J-7_correspondance_02.bed_GBM.txt > 1-J-7_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 2-J-2_correspondance_02.bed_GBM.txt > 2-J-2_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 2-V-620_correspondance_02.bed_GBM.txt > 2-V-620_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 3-V-613_correspondance_02.bed_GBM.txt > 3-V-613_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 1-R-180_correspondance_02.bed_GBM.txt > 1-R-180_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 2-J-7_correspondance_02.bed_GBM.txt > 2-J-7_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 3-J-2_correspondance_02.bed_GBM.txt > 3-J-2_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 3-V-620_correspondance_02.bed_GBM.txt > 3-V-620_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 1-R-183_correspondance_02.bed_GBM.txt > 1-R-183_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 2-R-180_correspondance_02.bed_GBM.txt > 2-R-180_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 3-J-7_correspondance_02.bed_GBM.txt > 3-J-7_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 1-V-613_correspondance_02.bed_GBM.txt > 1-V-613_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 2-R-183_correspondance_02.bed_GBM.txt > 2-R-183_correspondance_02.bed_GBM_vlookup.txt
+
+awk '
+    # { sub(/\r$/,"") }    # uncomment to remove Windows style line-endings.
+    NR==FNR{a[$1]          # hash $1 of genes file to a
+    next
+}
+($1 in a) {                # lookup from transcriptome
+    print
+}' Correspondance_scaff_08.txt 3-R-180_correspondance_02.bed_GBM.txt > 3-R-180_correspondance_02.bed_GBM_vlookup.txt
