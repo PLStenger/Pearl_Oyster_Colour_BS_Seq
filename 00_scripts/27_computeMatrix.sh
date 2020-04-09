3d6baa00ee3d20cf4c3d44829ddc093e218e1323
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-09 17:08:49 +1100

    Update 27_computeMatrix.sh

--- 00_scripts/27_computeMatrix.sh
+++ 00_scripts/27_computeMatrix.sh
@@ -13,5 +13,11 @@ source activate $DATAWORK/96_env_conda/deeptools
 
 
 #computeMatrix scale-regions -S Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw -R $REFERENCE --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o matrix.mat.gz
+#plotProfile -m matrix.mat.gz -out Index_10.2-V-620.pdf --plotTitle "Index_10.2-V-620" --plotHeight 10 --plotWidth 10
 
-plotProfile -m matrix.mat.gz -out Index_10.2-V-620.pdf --plotTitle "Index_10.2-V-620" --plotHeight 10 --plotWidth 10
+computeMatrix scale-regions -S Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_11.3-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_1.3-R-180_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_18.1-J-2_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_19.2-J-2_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_20.3-J-2_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_21.1-J-7_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_2.1-R-183_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_22.2-J-7_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_23.3-J-7_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_25.1-R-180_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_27.2-R-180_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_3.2-R-183_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_5.3-R-183_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw Index_6.1-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.bw Index_7.2-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.bw Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.bw Index_9.1-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw -R $REFERENCE --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o All_individuals_matrix.mat.gz
+
+plotProfile -m All_individuals_matrix.mat.gz -out All_individuals.pdf --plotTitle "All individuals" --plotHeight 10 --plotWidth 10
+
+
+
