911a5f85b7d82df3a884811ac83f88ade763940c
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-09 13:46:24 +1100

    Update 26_bamCoverage.sh

--- 00_scripts/26_bamCoverage.sh
+++ 00_scripts/26_bamCoverage.sh
@@ -10,4 +10,10 @@ cd $DATADIRECTORY
 export PATH=$PATH:/appli/anaconda/2.7/bin
 source activate $DATAWORK/96_env_conda/deeptools
 
-bamCoverage -b Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam -o Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw
+#bamCoverage -b Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam -o Index_10.2-V-620_R1_paired_bismark_bt2_pe.deduplicated.bam.sam_sorted_clean.bam.bw
+
+bamCoverage -b Index_6.1-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam -o Index_6.1-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.bw
+bamCoverage -b Index_7.2-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam -o Index_7.2-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.bw
+bamCoverage -b Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam -o Index_8.3-V-613_R1_paired_bismark_bt2_pe.deduplicated.bam_sorted_clean.bam.bw
+
+
