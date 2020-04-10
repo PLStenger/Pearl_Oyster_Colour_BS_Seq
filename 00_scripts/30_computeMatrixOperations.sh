2b1f78f5c82abb8915439f45dc1afe9d15875dc5
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-04-11 10:38:12 +1100

    Update 30_computeMatrixOperations.sh

--- 00_scripts/30_computeMatrixOperations.sh
+++ 00_scripts/30_computeMatrixOperations.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-#PBS -q mpi
-#PBS -l walltime=48:00:00
-#PBS -l select=1:ncpus=28:mem=115g
+#PBS -q omp
+#PBS -l walltime=180:00:00
+#PBS -l mem=1000g
+#PBS -l ncpus=64
 
 DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/06_bismark
 
