d2cb65f9fc1541a7cab8c7e7206f53678943713e
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2020-06-16 15:00:56 +1100

    Create zz_get_fasta.sh

---
+++ 00_scripts/zz_get_fasta.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+
+DATADIRECTORY=/home/datawork-ihpe/Pearl_Oyster_Colour_BS_Seq/01_genome
+
+# https://bedtools.readthedocs.io/en/latest/content/tools/getfasta.html
+
+BEDTOOLS_ENV=". /appli/bioinfo/bedtools/2.26.0/env.sh"
+
+$BEDTOOLS_ENV
+
+cd $DATADIRECTORY
+
+bedtools getfasta -fi sspace.final.scaffolds_02.fasta -gff Real_genes_with_intron_length.gff > Real_genes_with_intron_length.fasta
